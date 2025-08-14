08aac2cf12a17680e41600682ec4d4709c4467ad
Hirbod Mirjavadi <hm@nightstomp.com>
2025-08-14 12:45:35 +0300

    [expo-audio][expo-video] Add crossOrigin property to audio and video components for web

--- packages/expo-audio/build/Audio.types.d.ts
+++ packages/expo-audio/build/Audio.types.d.ts
@@ -53,6 +53,18 @@ export type AudioPlayerOptions = {
      * @platform android
      */
     downloadFirst?: boolean;
+    /**
+     * Determines the [cross origin policy](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Attributes/crossorigin) used by the underlying native view on web.
+     * If undefined (default), does not use CORS at all. If set to `'anonymous'`, the audio will be loaded with CORS enabled.
+     * Please note that some videos or audio may not play if CORS is enabled, depending on the CDN settings.
+     * If you encounter issues, consider adjusting the `crossOrigin` property.
+     *
+     * > **Note:** This prop is only available on web.
+     *
+     * @platform web
+     * @default undefined
+     */
+    crossOrigin?: 'anonymous' | 'use-credentials';
 };
 /**
  * @deprecated Use `AudioPlayerOptions` instead.
--- packages/expo-video/src/VideoView.types.ts
+++ packages/expo-video/src/VideoView.types.ts
@@ -163,10 +163,14 @@ export interface VideoViewProps extends ViewProps {
 
   /**
    * Determines the [cross origin policy](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Attributes/crossorigin) used by the underlying native view on web.
-   * If undefined, does not use CORS at all.
+   * If undefined (default), does not use CORS at all. If set to `'anonymous'`, the video will be loaded with CORS enabled.
+   * Please note that some videos or audio may not play if CORS is enabled, depending on the CDN settings.
+   * If you encounter issues, consider adjusting the `crossOrigin` property.
+   *
+   * > **Note:** This prop is only available on web.
    *
    * @platform web
-   * @default 'anonymous'
+   * @default undefined
    */
   crossOrigin?: 'anonymous' | 'use-credentials';
 }
